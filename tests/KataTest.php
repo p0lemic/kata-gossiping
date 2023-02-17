ef1956cd72798f1f709fa81f5edd9ded05a8793f
Javier Roman <javier.roman@drinksandco.com>
2023-02-17 18:05:46 +0100

    Repository creation

---
+++ tests/KataTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use Kata\Kata;
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+class KataTest extends TestCase
+{
+    public function testDummy()
+    {
+        $kata = new Kata();
+    }
+}
